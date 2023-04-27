deb4b7ec48b427b597be9b8fc9759eca9de4c9a5
Pritesh Singh <priteshsingh@Priteshs-MacBook-Air.local>
2023-04-27 17:06:38 +0530

    deploy

---
+++ src/pages/watch/Watch.jsx
@@ -0,0 +1,22 @@
+import { ArrowBackOutlined } from "@material-ui/icons";
+import { Link, useLocation } from "react-router-dom";
+import "./watch.scss";
+import VideoComponent from '../../components/videoPlayer/VideoComponent';
+
+
+export default function Watch() {
+  const location = useLocation();
+  const movie = location.movie;
+  return (
+    <div className="watch">
+      {console.log(movie)}
+      <Link to="/">
+        <div className="back">
+          <ArrowBackOutlined />
+          Home
+        </div>
+      </Link>
+      <VideoComponent encryptedUrl={movie.trailer} />
+    </div>
+  );
+}
